47adac06cc38d826d4fa0c6bc0ad7316fbb54737
Ra1ky <144536266+Ra1ky@users.noreply.github.com>
2025-04-06 22:06:28 +0300

    Add files via upload

---
+++ UpgradesMenu.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UpgradesMenu : MonoBehaviour
+{
+    //public PlayerStats playerStats;
+
+    public Image hpIndicator; 
+    public Image armorIndicator; 
+
+    public Sprite[] upgradeLevels; 
+
+    private int hpUpgradeLevel = 0;   
+    private int armorUpgradeLevel = 0; 
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
+    }
+
+    public void UpgradeHP()
+    {
+        if (hpUpgradeLevel < 3)
+        {
+            //playerStats.UpgradeHP();
+            hpUpgradeLevel++;
+            hpIndicator.sprite = upgradeLevels[hpUpgradeLevel];
+            //PlayerPrefs.SetInt("HPLevel", hpUpgradeLevel); 
+        }
+    }
+
+    public void UpgradeArmor()
+    {
+        if (armorUpgradeLevel < 3)
+        {
+            //playerStats.UpgradeArmor();
+            armorUpgradeLevel++;
+            armorIndicator.sprite = upgradeLevels[armorUpgradeLevel];
+            //PlayerPrefs.SetInt("ArmorLevel", armorUpgradeLevel);
+        }
+    }
+}
